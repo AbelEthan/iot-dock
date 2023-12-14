2911abe913a0995597d58de02b0b8891e9f7bcfa
AbelEthan <abelethan@vip.qq.com>
2023-12-14 18:32:35 +0800

    docs: 添加邮箱更新脚本

---
+++ gitemail.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+
+OLD_EMAIL="abelethan@126.com"
+CORRECT_NAME="AbelEthan"
+CORRECT_EMAIL="abelethan@vip.qq.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
